ccd8c22725663393dfbf1cfa4a64dcfaaddedfd2
Jorge Gabriel <jg3043505@gmail.com>
2025-09-19 19:16:16 -0300

    feat(Adicionar remoção individual e total)

--- src/js/index.js
+++ src/js/index.js
@@ -4,28 +4,72 @@ let tasks = [
     { id: 3, descripition: "Fazer exercícios", checked: false }
 ];
 
+const removeTask = (tasksId) => {
+    tasks = tasks.filter(({ id }) => parseInt(id) !== parseInt(tasksId));
+
+    document
+        .getElementById('todo-list')
+        .removeChild(document.getElementById(tasksId));
+
+}
+
+const removeDoneTasks = () => {
+    const tasksToRemove = tasks
+        .filter(({ checked }) => checked)
+        .map(({ id }) => id);
+        console.log(tasksToRemove);
+
+        tasks = tasks.filter(({ checked }) => !checked);
+
+        tasksToRemove.forEach((tasksToRemove) => {
+            document
+            .getElementById('todo-list')
+            .removeChild(document.getElementById(tasksToRemove))
+})
+}
+
 const createTaskListItem = (task, checkbox) => {
 
     const list = document.getElementById('todo-list');
     const toDo = document.createElement('li');
 
+    const removeTaskButon = document.createElement('button');
+    removeTaskButon.textContent = "X";
+    removeTaskButon.ariaLabel = 'Remover tarefa';
+
+    removeTaskButon.onclick = () => removeTask(task.id);
+
     toDo.id = task.id;
 
     toDo.appendChild(checkbox);
+    toDo.appendChild(removeTaskButon);
     list.appendChild(toDo);
     return toDo;
 }
 
+const onCheckboxClick = (event) => {
+    const [id] = event.target.id.split('-')[0];
+    
+    tasks = tasks.map((task) => {
+        return (parseInt(id) === parseInt(task.id)) 
+            ? { ...task, checked: event.target.checked }
+            : task;       
+})
+}
+console.log(tasks);
 
-const getCheckBoxInput = ({ id, descripition }) => {
+
+const getCheckBoxInput = ({ id, descripition, checked }) => {
     const checkbox = document.createElement('input');
     const label = document.createElement('label');
     const wrapper = document.createElement('div');
     const checkboxId = `${id}-checkbox`;
 
+    
     checkbox.type = 'checkbox';
     checkbox.id = checkboxId;
-    checkbox.checked = tasks.checked || false;
+    checkbox.checked = checked || false;
+    checkbox.addEventListener('change', onCheckboxClick);
 
     label.textContent = descripition;
     label.htmlFor = checkboxId;
@@ -51,33 +95,23 @@ const getNewTaskData = (event) => {
 
 const createTask = (event) => {
     event.preventDefault();
-
-    const newTaskData = getNewTaskData(event);
-    const { id, descripition } = newTaskData;
+    const newTaskData = getNewTaskData(event);    
 
 
     const checkbox = getCheckBoxInput(newTaskData)
     createTaskListItem(newTaskData, checkbox);
 
-    tasks + [...tasks, { id: newTaskData.id, descripition: newTaskData.descripition, checked: false }];
+    tasks.push({ id: newTaskData.id, descripition: newTaskData.descripition, checked: false });
 
 }
 
 window.onload = function () {
     const form = this.document.getElementById('create-todo-form');
-    form.addEventListener('submit', createTask)
+    form.addEventListener('submit', createTask);
 
     tasks.forEach((task) => {
-        const checkbox = getCheckBoxInput(task);
-
-        const list = document.getElementById('todo-list');
-        const toDo = document.createElement('li');
-        //const buttom = document.createElement('button');
-
-        toDo.id = task.id;
-        toDo.appendChild(checkbox);
-        //toDo.appendChild(buttom);
-
-        list.appendChild(toDo);
-    })
+       
+        const checkbox = getCheckBoxInput(task);      
+        createTaskListItem(task, checkbox);        
+    });
 }
